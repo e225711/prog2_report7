c9a3740037e25f6e626641d2d41f41141a45854d
Maimu Kohagura <e225711@ie.u-ryukyu.ac.jp>
2023-01-24 11:34:45 +0900

    1st commit

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int output_list(int array[], int size);
+int output_list(int array[], int size){
+  int i;
+  for (i=0 ; i < size ; i++)	{
+		printf("%d ", array[i]);
+	}
+  printf("\n");
+
+	return 0;
+}
+
+int descending_order(int array[], int size);
+int descending_order(int array[], int size){
+  int i,j,k;
+  int tmp;
+
+  int array_copy[size];
+
+  for (int k = 0; k < size; ++k) {
+        array_copy[k] = array[k];
+    }
+
+  for (i = 0; i < size; i++){
+    for(j = i + 1; j < 5; j++){
+      if (array_copy[i] < array_copy[j]){
+        tmp = array_copy[i];
+        array_copy[i] = array_copy[j];
+        array_copy[j] = tmp;
+      }
+    }
+  }
+
+  output_list(array_copy,size);
+
+  return 0;
+}
+
+int main(){
+    const int SIZE = 5; //定数として配列のサイズを宣言                          
+    int scores[SIZE] = {0, 60, 70, 100, 90};
+    //処理コード
+    
+    printf("scores = ");
+    output_list(scores,SIZE);
+
+    printf("results = ");
+    descending_order(scores,SIZE);
+
+    return 0;
+}
